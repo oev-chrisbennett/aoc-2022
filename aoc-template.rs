058711f860a003c240edc19d81acac326a35935a
Chris Bennett <chris.bennett@octopusev.com>
2022-12-01 18:22:26 +0000

    solution template 🤠

---
+++ aoc-template.rs
@@ -0,0 +1,20 @@
+
+
+use std::io::{self, Read};
+
+fn main() { 
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    
+    println!("Part 1 answer> {}", part1(&input));
+    println!("Part 2 answer> {}", part2(&input));
+}
+
+fn part1(input: &str) -> i64 {
+    1
+}
+
+
+fn part2(input: &str) -> i64 {
+    1
+}
