9a1542cf3bba052b91b431257a8d8d87b72c8b41
Chris Bennett <chris.bennett@octopusev.com>
2022-12-01 20:12:17 +0000

    Day 1 - Part 2

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     println!("Part 2 answer> {}", part2(&input));
 }
 
-pub fn part1(input: &str) -> u32 {
+fn get_sorted_totals(input: &str) -> Vec<u32> {
     let mut curr_total: u32 = 0;
     let mut result: Vec<u32> = vec![];
     for line in input.lines() {
@@ -27,12 +27,19 @@ pub fn part1(input: &str) -> u32 {
 
     result.push(curr_total);
     result.sort();
-
-    return result[result.len() - 1];
+    return result;
 }
 
-fn part2(_input: &str) -> u32 {
-    1
+fn part1(input: &str) -> u32 {
+    let sorted_totals: Vec<u32> = get_sorted_totals(&input);
+
+    return sorted_totals[sorted_totals.len() - 1];
+}
+
+fn part2(input: &str) -> u32 {
+    let mut sorted_totals: Vec<u32> = get_sorted_totals(&input);
+    let toptotal = sorted_totals.drain(sorted_totals.len() - 3..);
+    return toptotal.sum();
 }
 
 mod testing {
@@ -45,5 +52,10 @@ mod testing {
             part1("1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"),
             24000
         );
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("5000\n\n24000\n\n8000\n\n10000\n\n8900\n\n11000"), 45000);
     }
 }
