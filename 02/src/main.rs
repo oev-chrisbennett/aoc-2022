ce9fdf907f091adb68203d3f9e2bfac773bb4c92
Chris Bennett <chris.bennett@octopusev.com>
2022-12-02 16:27:07 +0000

    Day 2 - Part 2 ⭐️

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -40,7 +40,7 @@ impl Result {
     }
 }
 
-fn define_scores() -> HashMap<&'static str, u32> {
+fn define_scores_part1() -> HashMap<&'static str, u32> {
     let game_score = HashMap::from([
         ("A X", Move::Rock.value_of_moves() + Result::Draw.value_of_win()),
         ("A Y", Move::Paper.value_of_moves() + Result::Win.value_of_win()),
@@ -56,8 +56,24 @@ fn define_scores() -> HashMap<&'static str, u32> {
     return game_score;
 }
 
+fn define_scores_part2() -> HashMap<&'static str, u32> {
+    let game_score = HashMap::from([
+        ("A X", Move::Scissors.value_of_moves() + Result::Lose.value_of_win()),
+        ("B X", Move::Rock.value_of_moves() + Result::Lose.value_of_win()),
+        ("C X", Move::Paper.value_of_moves() + Result::Lose.value_of_win()),
+        ("A Y", Move::Rock.value_of_moves() + Result::Draw.value_of_win()),
+        ("B Y", Move::Paper.value_of_moves() + Result::Draw.value_of_win()),
+        ("C Y", Move::Scissors.value_of_moves() + Result::Draw.value_of_win()),
+        ("A Z", Move::Paper.value_of_moves() + Result::Win.value_of_win()),
+        ("B Z", Move::Scissors.value_of_moves() + Result::Win.value_of_win()),
+        ("C Z", Move::Rock.value_of_moves() + Result::Win.value_of_win()),
+    ]);
+
+    return game_score;
+}
+
 fn part1(input: &str) -> u32 {
-    let score: HashMap<&str, u32> = define_scores();
+    let score: HashMap<&str, u32> = define_scores_part1();
     let mut total: u32 = 0;
     for line in input.lines() {
         total += score[line];
@@ -66,8 +82,14 @@ fn part1(input: &str) -> u32 {
     total
 }
 
-fn part2(_input: &str) -> i64 {
-    1
+fn part2(input: &str) -> u32 {
+    let score: HashMap<&str, u32> = define_scores_part2();
+    let mut total: u32 = 0;
+    for line in input.lines() {
+        total += score[line];
+    }
+
+    total
 }
 
 mod testing {
@@ -81,6 +103,6 @@ mod testing {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2("5000\n\n24000\n\n8000\n\n10000\n\n8900\n\n11000"), 45000);
+        assert_eq!(part2("A Y\nB X\nC Z"), 12);
     }
 }
