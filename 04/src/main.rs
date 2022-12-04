1e8e631d3c331de3d08ea16709e76e7a18e8dbd8
Chris Bennett <chris.bennett@octopusev.com>
2022-12-04 16:20:56 +0000

    Day 4 - Part 2 ⭐️

--- 04/src/main.rs
+++ 04/src/main.rs
@@ -27,7 +27,7 @@ fn part1(input: &str) -> u32 {
 
         if elf_b.iter().all(|item: &u32| elf_a.contains(item)) {
             count += 1;
-        } else if elf_a.iter().all(|item| elf_b.contains(item)) {
+        } else if elf_a.iter().all(|item: &u32| elf_b.contains(item)) {
             count += 1;
         }
 
@@ -43,8 +43,39 @@ fn part1(input: &str) -> u32 {
     count
 }
 
-fn part2(_input: &str) -> u32 {
-    1
+fn part2(input: &str) -> u32 {
+    let mut count: u32 = 0;
+    for line in input.lines() {
+        let ranges: Vec<&str> = line.split(",").collect();
+        let elves: Vec<Vec<u32>> = ranges
+            .into_iter()
+            .map(|range: &str| {
+                let range_parts: Vec<&str> = range.split("-").collect();
+                let first: u32 = range_parts[0].parse::<u32>().unwrap();
+                let second: u32 = range_parts[1].parse::<u32>().unwrap();
+                vec![first, second]
+            })
+            .collect();
+
+        let elf_a: Vec<u32> = (elves[0][0]..elves[0][1] + 1).collect();
+        let elf_b: Vec<u32> = (elves[1][0]..elves[1][1] + 1).collect();
+
+        if elf_b.iter().any(|item: &u32| elf_a.contains(item)) {
+            count += 1;
+        } else if elf_a.iter().any(|item: &u32| elf_b.contains(item)) {
+            count += 1;
+        }
+
+        // -- Debug --
+        // println!(
+        //     "{:?}\t{:?}\t{:?}\t{}",
+        //     elf_a.iter().all(|item| elf_b.contains(item)),
+        //     elf_b.iter().all(|item| elf_a.contains(item)),
+        //     elves,
+        //     count
+        // );
+    }
+    count
 }
 
 mod testing {
@@ -58,6 +89,6 @@ mod testing {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(""), 0);
+        assert_eq!(part2("2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"), 4);
     }
 }
